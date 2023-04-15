fab386c8761e7c2e84cb46a58818a9835601ae66
water_stop <3045672234@qq.com>
2023-04-15 22:59:25 +0800

    end

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,16 +1,14 @@
 // move_semantics1.rs
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = Vec::new(); // 在堆上new一个vector
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // 调用函数
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
-    vec1.push(88);
+    vec1.push(88); // 上面定义vec1是一个不可变调用
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
@@ -18,9 +16,9 @@ fn main() {
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
-    vec.push(22);
+    vec.push(22); // 相当于Vec::push(&mut vec, 22)，其中&mut vec为一个可变借用
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec // 无分号表达式的返回
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -6,7 +6,7 @@
 // - Modules
 // - Enums
 
-// Let's build a little machine in form of a function.
+// Let's build a little machine in the form of a function.
 // As input, we're going to give a list of strings and commands. These commands
 // determine what action is going to be applied to the string. It can either be:
 // - Uppercase the string
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -30,21 +28,25 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_string().to_uppercase()),
+                Command::Trim => output.push(string.to_string().trim().to_string()),
+                Command::Append(n) => output.push(string.clone() + "bar".repeat(*n).as_str()),
+            }
         }
         output
     }
 }
-
 #[cfg(test)]
 mod tests {
-    // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    // TODO: What do we need to import to have `transformer` in scope?
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
